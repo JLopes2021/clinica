d578158d7533e83d8e873c8b29992bce73dca1ea
JLopes2021 <jeffinho.je8@gmail.com>
2023-06-29 19:57:11 -0300

    aplicacao-total-funcional

--- Index.php
+++ Index.php
@@ -13,32 +13,9 @@
     
 </head>
 <body>
-    <!-- Barra de navegação -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <h1>Logo aqui</h1>
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent" >
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <!-- Links da barra de navegação -->
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="./Index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="?page=novo">Novo Usuário</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="?page=listar">Lista de Usuários</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="?page=listarC">Listar Consultas</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+   <?php 
+   include("./layout/navbar.php");
+   ?>
 
     <div class="container">
         <div class="row">
@@ -82,6 +59,9 @@
         </div>
     </div>
 
+    <?php 
+   include("./layout/footer.php");
+   ?>
     <!-- Inclusão do arquivo JS do Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
---
+++ layout/navbar.php
@@ -0,0 +1,26 @@
+ <!-- Barra de navegação -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <h1>Logo aqui</h1>
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <!-- Links da barra de navegação -->
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="./Index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?page=novo">Novo Usuário</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?page=listar">Lista de Usuários</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?page=listarC">Listar Consultas</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
